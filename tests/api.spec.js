8396e5d75d3666ea86109ad7e6d35be937c12cc3
AnaMigriauli <anamigriauli1994@gmail.com>
2025-03-13 14:34:45 +0400

    Removed try-catch block and updated test names

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -8,91 +8,62 @@ const apiURL2 = `https://api.clickup.com/api/v2/task/${listID}`;
 let createdTaskId;
 
 test.describe.serial("ClickUp API Tests", () => {
-  test("Create a Task", async () => {
-    try {
-      const taskData = {
-        name: "test1",
-        description: "test1 Task",
-        status: "to do",
-      };
+  test("If user creates a task, it should be created successfully", async () => {
+    const taskData = {
+      name: "test1",
+      description: "test1 Task",
+      status: "to do",
+    };
 
-      const response = await axios.post(apiUrl, taskData, {
+    const response = await axios.post(apiUrl, taskData, {
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.data).toHaveProperty("id");
+
+    createdTaskId = response.data.id;
+  });
+
+  test("If user retrieves task data, it should return correct details", async () => {
+    const response = await axios.get(`${apiURL2}/${createdTaskId}`, {
+      headers: { Authorization: token },
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.data.name).toBe("test1");
+    expect(response.data.description).toBe("test1 Task");
+  });
+
+  test("If user updates a task, it should update correctly", async () => {
+    const UpdateTaskData = {
+      name: "test2",
+      description: "test2 Task",
+      status: "In progress",
+    };
+
+    const response = await axios.put(
+      `${apiURL2}/${createdTaskId}`,
+      UpdateTaskData,
+      {
         headers: {
           Authorization: token,
-          "Content-Type": "application/json",
         },
-      });
+      }
+    );
 
-      expect(response.status).toBe(200);
-      expect(response.data).toHaveProperty("id");
-
-      createdTaskId = response.data.id;
-    } catch (error) {
-      console.error(
-        "Error during the Task creation",
-        error.response?.data || error.message
-      );
-    }
+    expect(response.status).toBe(200);
+    expect(response.data.name).toBe("test2");
+    expect(response.data.description).toBe("test2 Task");
   });
 
-  test("Get Task data", async () => {
-    try {
-      const response = await axios.get(`${apiURL2}/${createdTaskId}`, {
-        headers: { Authorization: token },
-      });
-
-      console.log(response.data);
-      expect(response.status).toBe(200);
-      expect(response.data.name).toBe("test1");
-      expect(response.data.description).toBe("test1 Task");
-    } catch (error) {
-      console.error(
-        "Error during the Task data retriev",
-        error.response?.data || error.message
-      );
-    }
-  });
-
-  test("Update task", async () => {
-    try {
-      const UpdateTaskData = {
-        name: "test2",
-        description: "test2 Task",
-        status: "In progress",
-      };
-
-      const response = await axios.put(
-        `${apiURL2}/${createdTaskId}`,
-        UpdateTaskData,
-        {
-          headers: {
-            Authorization: token,
-          },
-        }
-      );
-
-      expect(response.status).toBe(200);
-      expect(response.data.name).toBe("test2");
-      expect(response.data.description).toBe("test2 Task");
-    } catch (error) {
-      console.error(
-        "Error during the Task update",
-        error.response?.data || error.message
-      );
-    }
-  });
-
-  test("Delete task", async () => {
-    try {
-      const response = await axios.delete(`${apiURL2}/${createdTaskId}`, {
-        headers: { Authorization: token },
-      });
-      expect(response.status).toBe(204);
-    } catch (error) {
-      console.error(
-        "Error during the Task deletion",
-        error.response?.data || error.message
-      );
-    }
+  test("If user deletes a task, it should be removed successfully", async () => {
+    const response = await axios.delete(`${apiURL2}/${createdTaskId}`, {
+      headers: { Authorization: token },
+    });
+    expect(response.status).toBe(204);
   });
 });
